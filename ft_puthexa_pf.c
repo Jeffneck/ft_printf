2b9317a742adf877064f935818c11d0323ffe02b
Hugo PC portable <hugot.anglade@gmail.com>
2023-09-13 23:54:21 +0200

    modif puthexa

--- ft_puthexa_pf.c
+++ ft_puthexa_pf.c
@@ -1,44 +1,40 @@
 #include "ft_printf.h"
-//attention printf bug avec les hexa negatifs (unsigned int)
-void ft_putchar_pf(char c, size_t *count_char){
-    if(count_char == NULL){
-        return;
-    }
-    write(1, &c, 1);
-    (*count_char)++;
-}
+//attention printf marche bizarrement avec les hexa negatifs 
+//à cause de la manière dont ils sont rpz en mémoire (complément à 2)
+//on utilise des unsigned int pour que cela fonctionne de la mm manière.
 
-void ft_b10_to_b16_pf(unsigned int nb, size_t *count_char){
+void ft_b10_to_b16_pf(unsigned int nb, int *p_count){
     char b16[] = "0123456789abcdef"; 
-    ft_putchar_pf(b16[nb], count_char);
+    ft_putchar_pf(b16[nb], p_count);
 
 }
-void ft_puthexa_pf(unsigned int nb, size_t *count_char){
-    if (count_char == NULL)
+void ft_puthexa_pf(unsigned int nb, int *p_count)
+{
+    if (p_count == NULL)
         return; 
     
     if (nb >= 0 && nb <= 16){
-        ft_b10_to_b16_pf(nb, count_char);
+        ft_b10_to_b16_pf(nb, p_count);
     }
     
     if (nb > 16){
-        ft_puthexa_pf((nb / 16), count_char);
-        ft_puthexa_pf((nb % 16), count_char);
+        ft_puthexa_pf((nb / 16), p_count);
+        ft_puthexa_pf((nb % 16), p_count);
     }
 
 }
 
-int main()
-{
-    size_t count_char; 
+// int main()
+// {
+//     size_t count_char; 
 
-    count_char = 0;
-    //ft_puthexa_pf(1, &count_char);
-    ft_puthexa_pf(22, &count_char);
-    //ft_puthexa_pf(45, &count_char);
-    //ft_puthexa_pf(2147483647, &count_char);
-    //ft_puthexa_pf(5, &count_char);
-    //ft_puthexa_pf(-2147483648, &count_char);
-    printf("\n count_char = %d", count_char);
-    return 0;
-}
+//     count_char = 0;
+//     //ft_puthexa_pf(1, &count_char);
+//     ft_puthexa_pf(22, &count_char);
+//     //ft_puthexa_pf(45, &count_char);
+//     //ft_puthexa_pf(2147483647, &count_char);
+//     //ft_puthexa_pf(5, &count_char);
+//     //ft_puthexa_pf(-2147483648, &count_char);
+//     printf("\n count_char = %d", count_char);
+//     return 0;
+// }
